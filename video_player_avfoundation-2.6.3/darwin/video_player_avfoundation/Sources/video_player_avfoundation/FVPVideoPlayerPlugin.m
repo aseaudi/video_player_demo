95aa8a7c7037c20ce6eb54d27341dc2d88c4d9e7
Abdelmuhaimen Seaudi <aseaudi@gmail.com>
2025-02-01 22:22:08 +0200

    1

--- video_player_avfoundation-2.6.3/darwin/video_player_avfoundation/Sources/video_player_avfoundation/FVPVideoPlayerPlugin.m
+++ video_player_avfoundation-2.6.3/darwin/video_player_avfoundation/Sources/video_player_avfoundation/FVPVideoPlayerPlugin.m
@@ -334,10 +334,10 @@ NS_INLINE CGFloat radiansToDegrees(CGFloat radians) {
   // self.customQueue = dispatch_queue_create("resourceLoaderQueue", DISPATCH_QUEUE_SERIAL);
   // self.session = [NSURLSession sessionWithConfiguration:config delegate:self delegateQueue:self.customQueue];
   // dispatch_get_main_queue() means that the delegate methods will be executed on the main thread.
-  // [urlAsset.resourceLoader setDelegate:self queue:dispatch_get_main_queue()];
+  [urlAsset.resourceLoader setDelegate:self queue:dispatch_get_main_queue()];
   
   // Set the delegate with a nil queue (defaults to the main queue)
-  [urlAsset.resourceLoader setDelegate:self queue:nil];
+  // [urlAsset.resourceLoader setDelegate:self queue:nil];
 
   // Create a custom dispatch queue (with a specific name for debugging purposes)
   // self.customQueue = dispatch_queue_create("resourceLoaderQueue", DISPATCH_QUEUE_SERIAL);
